7c46486cf30f0ee093c7f22398cae11b8e99600d
DrWhax <drwhax@riseup.net>
2018-06-11 10:21:09 -0400

    new init.el

---
+++ init.el
@@ -0,0 +1,118 @@
+(require 'package)
+(add-to-list 'package-archives
+         '("melpa" . "https://melpa.org/packages/") t)
+
+(package-initialize)
+
+(when (not package-archive-contents)
+    (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(add-to-list 'load-path "~/.emacs.d/custom")
+
+(require 'setup-general)
+(if (version< emacs-version "24.4")
+    (require 'setup-ivy-counsel)
+  (require 'setup-helm)
+  (require 'setup-helm-gtags))
+;; (require 'setup-ggtags)
+(require 'setup-cedet)
+(require 'setup-editing)
+
+;; elpy
+(package-initialize)
+(elpy-enable)
+
+;; neotree
+(add-to-list 'load-path "~/.emacs.d/elpa/neotree-20170522.758")
+(require 'neotree)
+(global-set-key [f8] 'neotree-toggle)
+
+
+;; theme stuff
+(add-to-list 'custom-theme-load-path "~/.emacs.d/themes")
+(load-theme 'dracula t)
+
+
+;; function-args
+;; (require 'function-args)
+;; (fa-config-default)
+;; (define-key c-mode-map  [(tab)] 'company-complete)
+;; (define-key c++-mode-map  [(tab)] 'company-complete)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (zygospore helm-gtags helm yasnippet ws-butler volatile-highlights use-package undo-tree iedit dtrt-indent counsel-projectile company clean-aindent-mode anzu))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
+
+;; Basic .emacs with a good set of defaults, to be used as template for usage
+;; with OCaml and OPAM
+;;
+;; Author: Louis Gesbert <louis.gesbert@ocamlpro.com>
+;; Released under CC0
+
+;; Generic, recommended configuration options
+
+(custom-set-variables
+ '(indent-tabs-mode nil)
+ '(compilation-context-lines 2)
+ '(compilation-error-screen-columns nil)
+ '(compilation-scroll-output t)
+ '(compilation-search-path (quote (nil "src")))
+ '(electric-indent-mode nil)
+ '(next-line-add-newlines nil)
+ '(require-final-newline t)
+ '(sentence-end-double-space nil)
+ '(show-trailing-whitespace t)
+ '(visible-bell t)
+ '(show-paren-mode t)
+ '(next-error-highlight t)
+ '(next-error-highlight-no-select t)
+ '(backup-directory-alist '(("." . "~/.local/share/emacs/backups")))
+ '(ac-use-fuzzy nil)
+ '(line-move-visual t)
+ )
+
+;; ANSI color in compilation buffer
+(require 'ansi-color)
+(defun colorize-compilation-buffer ()
+  (toggle-read-only)
+  (ansi-color-apply-on-region (point-min) (point-max))
+  (toggle-read-only))
+(add-hook 'compilation-filter-hook 'colorize-compilation-buffer)
+
+;; Some key bindings
+
+(global-set-key [f3] 'next-match)
+(defun prev-match () (interactive nil) (next-match -1))
+(global-set-key [(shift f3)] 'prev-match)
+(global-set-key [backtab] 'auto-complete)
+;; OCaml configuration
+;;  - better error and backtrace matching
+
+(defun set-ocaml-error-regexp ()
+  (set
+   'compilation-error-regexp-alist
+   (list '("[Ff]ile \\(\"\\(.*?\\)\", line \\(-?[0-9]+\\)\\(, characters \\(-?[0-9]+\\)-\\([0-9]+\\)\\)?\\)\\(:\n\\(\\(Warning .*?\\)\\|\\(Error\\)\\):\\)?"
+    2 3 (5 . 6) (9 . 11) 1 (8 compilation-message-face)))))
+
+(add-hook 'tuareg-mode-hook 'set-ocaml-error-regexp)
+(add-hook 'caml-mode-hook 'set-ocaml-error-regexp)
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
+
